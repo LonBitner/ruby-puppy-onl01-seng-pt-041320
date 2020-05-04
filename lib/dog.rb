595c2c27d228d966509fe21e7e03fa869031f37c
Lon Bitner <lonbitner@gmail.com>
2020-05-04 21:51:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,7 +12,7 @@ class Dog
   end
 
   def self.all
-   @@all
+    @@all
   end
 
   def self.clear_all
@@ -20,8 +20,8 @@ class Dog
   end
   
   def self.print_all
-    @@all.each do |names|
-      puts names
+    @@all.each do |dog|
+      puts dog.name
     end
   end
   
